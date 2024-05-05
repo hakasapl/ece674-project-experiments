8fbd02b6ed748a88188bffb61fbfe32364910400
hakasapl <hsaplakoglu@umass.edu>
2024-05-05 20:08:31 +0000

    added sleep to run-everything

--- run-everything.sh
+++ run-everything.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ "$EUID" -ne 0 ]
   then echo "This script must be run as root"
   exit
@@ -13,16 +14,22 @@ bg_pid=$!
 
 # Step Benchmark CPU
 ./step-benchmark-cpu.sh $output_folder/log-step-cpu.csv 60
+sleep 60
 # Step Benchmark Mem
 ./step-benchmark-mem.sh $output_folder/log-step-mem.csv 60
+sleep 60
 # Step Benchmark
 ./step-benchmark.sh $output_folder/log-step.csv 60
+sleep 60
 
 # Step Powerlim CPU
 ./step-powerlim-benchmark-cpu.sh $output_folder/log-step-powerlim-cpu.csv 60
+sleep 60
 # Step Powerlim Mem
 ./step-powerlim-benchmark-mem.sh $output_folder/log-step-powerlim-mem.csv 60
+sleep 60
 # Step Powerlim
 ./step-powerlim-benchmark.sh $output_folder/log-step-powerlim.csv 60
+sleep 60
 
 kill $bg_pid
